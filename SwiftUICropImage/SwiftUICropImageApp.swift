b2dfab08f8f40007a996fad8aa1faf507564204d
sanhee16 <sinhioa20@gmail.com>
2024-01-15 20:27:51 +0900

    🎉Init: Init New Project

---
+++ SwiftUICropImage/SwiftUICropImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICropImageApp.swift
+//  SwiftUICropImage
+//
+//  Created by sandy on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICropImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
